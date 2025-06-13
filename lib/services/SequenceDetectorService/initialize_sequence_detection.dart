69578df03ae35bfa176b7fb38810ff91a717250e
mozrin <mozrin@gmail.com>
2025-06-14 00:04:29 +0700

    change: removed unused sequences

--- lib/services/SequenceDetectorService/initialize_sequence_detection.dart
+++ lib/services/SequenceDetectorService/initialize_sequence_detection.dart
@@ -5,12 +5,6 @@ void initializeSequenceDetection(BuildContext context) {
   const int sequenceDetectionTimeout = 5;
   SequenceDetectorService()
       .setTimeframe(const Duration(seconds: sequenceDetectionTimeout));
-  SequenceDetectorService().registerPattern([0, 1, 0], () {
-    mozPrint('0-1-0 pattern triggered', 'SEQUENCE', 'TRIGGER');
-  });
-  SequenceDetectorService().registerPattern([4, 3, 2, 1, 0], () {
-    mozPrint('4-3-2-1-0 pattern triggered', 'SEQUENCE', 'TRIGGER');
-  });
   SequenceDetectorService().registerPattern([0, 1, 2, 3, 4, 4, 4], () {
     mozPrint('0-1-2-3-4-4-4 pattern triggered. Showing config dialog.',
         'SEQUENCE', 'TRIGGER');
