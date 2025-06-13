8e7653146e8dd70022e6f73a4e17ac3f9827fa0e
mozrin <mozrin@gmail.com>
2025-06-13 09:06:25 +0700

    refactor: for the widgets.dart exports

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,3 @@
 export 'package:devlite_flutter/widgets/footer_navigation.dart';
 export 'package:devlite_flutter/widgets/header_navigation.dart';
-export 'package:devlite_flutter/widgets/app_modal_dialog.dart';
+export 'package:devlite_flutter/widgets/dialog_modal_yes_no.dart';
