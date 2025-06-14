d6be30244e85a6e347bf100d2d77556934360c35
mozrin <mozrin@gmail.com>
2025-06-14 09:07:07 +0700

    change: refactor navigation widgets

---
+++ lib/widgets/navigation/navigation.dart
@@ -0,0 +1,3 @@
+export 'package:devlite_flutter/widgets/navigation/footer/footer_navigation.dart';
+export 'package:devlite_flutter/widgets/navigation/header/header_navigation.dart';
+export 'package:devlite_flutter/widgets/navigation/header/header_language_selector.dart';
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,2 @@
-export 'package:devlite_flutter/widgets/footer_navigation.dart';
-export 'package:devlite_flutter/widgets/header_navigation.dart';
 export 'package:devlite_flutter/widgets/dialogs/dialogs.dart';
-export 'package:devlite_flutter/widgets/header_language_selector.dart';
+export 'package:devlite_flutter/widgets/navigation/navigation.dart';
